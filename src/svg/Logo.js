dbbe197b5b9ddea3ffe415d2a23cceaf48006615
SkySails <malte.hallstrom@gmail.com>
2020-09-20 23:01:45 +0200

    Added logo

---
+++ src/svg/Logo.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo-graphics">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        viewBox="0 0 58 59"
+      >
+        <defs>
+          <linearGradient
+            id="linear-gradient"
+            x1="0.5"
+            x2="0.5"
+            y2="1"
+            gradientUnits="objectBoundingBox"
+          >
+            <stop offset="0" stopColor="#0067ee" stopOpacity="0" />
+            <stop offset="1" stopColor="#0067ee" />
+          </linearGradient>
+        </defs>
+        <g
+          id="Group_252"
+          data-name="Group 252"
+          transform="translate(-18.166 -11.854)"
+        >
+          <rect
+            id="Rectangle_31"
+            data-name="Rectangle 31"
+            width="58"
+            height="59"
+            rx="10"
+            transform="translate(18.166 11.854)"
+            fill="#0067ee"
+          />
+          <path
+            id="Intersection_1"
+            data-name="Intersection 1"
+            d="M-4049.776,187.907a22.733,22.733,0,0,0-3.819-.533c-.5,0-.849-3.06-.4-3.448a18.543,18.543,0,0,1,3.146-.44c1.619-.146,3.126-.318,3.351-.383l.408-.119-1.544-11.747-18.376-3.163-.354-1.16c-.684-2.248-.644-3.436.135-3.95.28-.185,2.16-.342,8.13-.673,7.539-.419,7.8-.443,8.842-.87a9.244,9.244,0,0,0,1.274-.6c.13-.1.2-.62.2-1.481a17.864,17.864,0,0,1,.842-4.944.653.653,0,0,1,.472-.321c.081,0,.389-.016.389-.016s.093-.558.14-.689c.391-1.108.511-1.247.707-1.247s.311.139.7,1.247c.046.131.139.689.139.689s.313.016.394.016a.655.655,0,0,1,.473.321,17.866,17.866,0,0,1,.842,4.944c0,.861.069,1.376.2,1.481a9.291,9.291,0,0,0,1.274.6c1.042.426,1.3.451,8.842.87,5.97.332,7.85.488,8.13.673.779.514.819,1.7.134,3.95l-.354,1.16-18.377,3.163-1.543,11.747.408.119c.225.064,1.732.237,3.351.383a18.553,18.553,0,0,1,3.147.44c.45.388.1,3.448-.4,3.448a22.741,22.741,0,0,0-3.819.533,21.035,21.035,0,0,1-3.544.44A21.021,21.021,0,0,1-4049.776,187.907Z"
+            transform="translate(4093.35 -136.151)"
+            fill="#fff"
+          />
+          <rect
+            id="Rectangle_32"
+            data-name="Rectangle 32"
+            width="46"
+            height="35"
+            transform="translate(24.166 27.854)"
+            fill="url(#linear-gradient)"
+          />
+          <text
+            id="STATS"
+            transform="translate(28.166 64.854)"
+            fill="#fff"
+            fontSize="12"
+            fontFamily="Poppins-Bold, Poppins"
+            fontWeight="700"
+          >
+            <tspan x="0" y="0">
+              STATS
+            </tspan>
+          </text>
+        </g>
+      </svg>
+    </div>
+  );
+}
